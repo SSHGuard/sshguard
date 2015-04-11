a06e523510c8c57e4e16f7d87cdc144ddadc3721
Kevin Zheng <kevinz5000@gmail.com>
2015-04-11 18:59:56 -0500

    Tweak test program name and improve usability

--- src/sshg_parser.c
+++ src/sshg_parser.c
@@ -1,20 +1,48 @@
+/**
+ * @file
+ * Parse and display individual attacks from standard input
+ */
+
 #include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "parser/parser.h"
 #include "sshguard_log.h"
 
-int main() {
+static void print_attack(const attack_t *attack) {
+    printf("attack from %s on service %d with danger %d with pid %d\n",
+            attack->address.value, attack->service,
+            attack->dangerousness, attack->source);
+}
+
+static void print_usage() {
+    fprintf(stderr, "usage: sshg-parser [-h] [-v]\n");
+}
+
+int main(int argc, char *argv[]) {
+    bool debug = false;
     char buf[1000];
+    int flag;
+
+    while ((flag = getopt(argc, argv, "hv")) != -1) {
+        switch (flag) {
+        case 'h':
+            print_usage();
+            return 0;
+        case 'v':
+            debug = true;
+        }
+    }
 
     // Initialize necessary subsystems.
     sshguard_log_init(true);
+    yydebug = yy_flex_debug = debug;
 
     while (fgets(buf, sizeof(buf), stdin) != NULL) {
         attack_t attack;
         if (parse_line(0, buf, &attack) == 0) {
-            printf("Attack from %s on service %d\n",
-                    attack.address.value, attack.service);
+            print_attack(&attack);
         }
     }
 }
