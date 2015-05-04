5a115a65055b3b9a5339b47b2530711859699a0a
Kevin Zheng <kevinz5000@gmail.com>
2015-05-04 15:43:51 -0500

    Fix build for 'ipfw' and 'hosts' backends
    Reported by:    Mark Felder <feld@FreeBSD.org>

--- src/fwalls/hosts.c
+++ src/fwalls/hosts.c
@@ -31,6 +31,7 @@
 #include "../config.h"
 #include "../sshguard_log.h"
 #include "../sshguard_fw.h"
+#include "parser/address.h"
 
 #ifndef HOSTSFILE_PATH
 #   define HOSTSFILE_PATH     "/etc/hosts.allow"
--- src/fwalls/ipfw.c
+++ src/fwalls/ipfw.c
@@ -33,6 +33,7 @@
 #include "../config.h"
 #include "../sshguard_log.h"
 #include "../sshguard_fw.h"
+#include "parser/address.h"
 
 #define IPFWMOD_ADDRESS_BULK_REPRESENTATIVE     "FF:FF:FF:FF:FF:FF:FF:FF"
 
