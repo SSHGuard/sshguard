0fe1dfc99d3c92247216785b39c320a8655647ab
Kevin Zheng <kevinz5000@gmail.com>
2016-07-24 11:50:55 -0700

    Be quiet when there are no errors to report

--- src/fwalls/pf.sh
+++ src/fwalls/pf.sh
@@ -1,17 +1,17 @@
 fw_init() {
-    pfctl -t sshguard -T show > /dev/null
+    pfctl -q -t sshguard -T show > /dev/null
 }
 
 fw_block() {
-    pfctl -k $1 -t sshguard -T add $1
+    pfctl -q -k $1 -t sshguard -T add $1
 }
 
 fw_release() {
-    pfctl -t sshguard -T del $1
+    pfctl -q -t sshguard -T del $1
 }
 
 fw_flush() {
-    pfctl -t sshguard -T flush
+    pfctl -q -t sshguard -T flush
 }
 
 fw_fin() {
