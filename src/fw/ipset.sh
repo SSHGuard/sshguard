c3cfbe5381ceb4a57e5281d69647a41fdeac787b
Daniel Aleksandersen <code@daniel.priv.no>
2016-11-18 18:33:14 +0100

    Add new ipset backend

---
+++ src/fw/ipset.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# sshg-fw-ipset
+# This file is part of SSHGuard.
+
+fw_init() {
+    ipset -quiet create -exist sshguard4 hash:ip family=inet
+    ipset -quiet create -exist sshguard6 hash:ip family=inet6
+}
+
+fw_block() {
+    ipset -quiet add -exist sshguard$2 $1
+}
+
+fw_release() {
+    ipset -quiet del -exist sshguard$2 $1
+}
+
+fw_flush() {
+    ipset -quiet flush sshguard4
+    ipset -quiet flush sshguard6
+}
+
+fw_fin() {
+    :
+}
