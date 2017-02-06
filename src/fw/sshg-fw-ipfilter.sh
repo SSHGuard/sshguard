e7ca8da50781e7eb4abc7e92a30bb92e59ead6e8
Kevin Zheng <kevinz5000@gmail.com>
2017-02-06 14:17:03 -0800

    Dynamically add rules to running ipfilter
    Submitted by:   Greg A. Woods

--- src/fw/sshg-fw-ipfilter.sh
+++ src/fw/sshg-fw-ipfilter.sh
@@ -2,42 +2,33 @@
 # sshg-fw-ipfilter
 # This file is part of SSHGuard.
 
+IPFILTER_CMD=ipf
 IPFILTER_CONF=/etc/ipfilter.conf
-IPFILTER_PATH=/sbin/ipf
 
 fw_init() {
-    grep -E '^##sshguard-begin##\n##sshguard-end##$' \
-        < ${IPFILTER_CONF} >/dev/null 2>/dev/null
+    :
 }
 
 fw_block() {
-    if test $SSHG_ADDRKIND != 4; then
-        return 1
+    FAM=""
+    if [ -n "$2" ]; then
+        FAM="-$2"
     fi
-    TMP=`mktemp /tmp/ipfconf.XXXXX`
-    awk '1 ; /^##sshguard-begin##$/ { print \"block in quick proto tcp from '\"$SSHG_ADDR\"' to any\" }' < ${IPFILTER_CONF} > $TMP && \
-        mv $TMP ${IPFILTER_CONF} && \
-        ${IPFILTER_PATH} -Fa && \
-        ${IPFILTER_PATH} -f ${IPFILTER_CONF}
+    echo "block in quick proto tcp from $1 to any" | \
+        ${IPFILTER_CMD} ${FAM} -A -f -
 }
 
 fw_release() {
-    if test $SSHG_ADDRKIND != 4; then
-        return 1
+    FAM=""
+    if [ -n "$2" ]; then
+        FAM="-$2"
     fi
-    TMP=`mktemp /tmp/ipfconf.XXXXX`
-    awk 'BEGIN { copy = 1 } copy ; /^##sshguard-begin##$/    { copy = 0 ; next } !copy { if ($0 !~ /'\"$SSHG_ADDR\"'.* /) print $0 } /^##sshguard-end##$/  { copy = 1 }' < ${IPFILTER_CONF} >$TMP && \
-        mv $TMP ${IPFILTER_CONF} && \
-        ${IPFILTER_PATH} -Fa && \
-        ${IPFILTER_PATH} -f ${IPFILTER_CONF}
+    echo "block in quick proto tcp from $1 to any" | \
+        ${IPFILTER_CMD} ${FAM} -A -r -f -
 }
 
 fw_flush() {
-    TMP=`mktemp /tmp/ipfconf.XXXXX`
-    awk 'BEGIN { copy = 1 } /^##sshguard-begin##$/ { print $0 ; copy = 0 } /^##sshguard-end##$/ { copy = 1 } copy' <${IPFILTER_CONF} >$TMP && \
-        mv $TMP ${IPFILTER_CONF} && \
-        ${IPFILTER_PATH} -Fa && \
-        ${IPFILTER_PATH} -f ${IPFILTER_CONF}
+    ${IPFILTER_CMD} -Fa && ${IPFILTER_CMD} -f ${IPFILTER_CONF}
 }
 
 fw_fin() {
